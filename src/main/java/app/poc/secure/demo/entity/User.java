c451ce4889d36ce32f6514db5b75c516b3d43fcd
s4b3l0 <sabelo.nkosi@rocketmail.com>
2025-05-14 07:43:09 +0200

    initial commit
    user class

---
+++ src/main/java/app/poc/secure/demo/entity/User.java
@@ -0,0 +1,32 @@
+package app.poc.secure.demo.entity;
+
+public class User {
+
+    private Long id;
+    private String username;
+    private String password;
+
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
