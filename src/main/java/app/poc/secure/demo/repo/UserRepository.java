291e1232c9b8236b1d1e5778f22ab77eb03ffa7f
s4b3l0 <sabelo.nkosi@rocketmail.com>
2025-05-15 18:38:41 +0200

    busy with user service

--- src/main/java/app/poc/secure/demo/repo/UserRepository.java
+++ src/main/java/app/poc/secure/demo/repo/UserRepository.java
@@ -3,6 +3,9 @@ package app.poc.secure.demo.repo;
 import app.poc.secure.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsDistinctByUsernameAndPassword(String username, String password);
+    Optional<User> findDistinctByUsername(String username);
 }
--- src/main/java/app/poc/secure/demo/service/UserService.java
+++ src/main/java/app/poc/secure/demo/service/UserService.java
@@ -1,7 +1,51 @@
 package app.poc.secure.demo.service;
 
+import app.poc.secure.demo.entity.User;
+import app.poc.secure.demo.repo.UserRepository;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @SneakyThrows
+    public User save(User user) {
+        //checking if username already registered in the db
+        var userOptional = userRepository.findDistinctByUsername(user.getUsername());
+
+        if (userOptional.isPresent()) {
+            log.warn("User {} already exists", user.getUsername());
+            return null;
+        }
+
+        log.info("Creating user {}", user.getUsername());
+        return userRepository.save(user);
+    }
+
+    @SneakyThrows
+    public Boolean grantUserAccess(String username, String password) {
+        return userRepository.existsDistinctByUsernameAndPassword(username, password);
+    }
+
+    @SneakyThrows
+    public Boolean changePassword(String username, String newPassword) {
+        var userOptional = userRepository.findDistinctByUsername(username);
+
+        if (userOptional.isPresent()) {
+            var user = userOptional.get();
+            user.setPassword(newPassword);
+            return userRepository.save(user) != null;
+        }
+
+        return false;
+    }
+
+
 }
